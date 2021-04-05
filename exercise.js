6a04e4727904aabb2a86508ddf693517dee48187
hzaybr <ahyebear@gmail.com>
2021-04-05 21:42:57 +0800

    add animation

---
+++ exercise.js
@@ -0,0 +1,14 @@
+$('.pic').click(function() {
+  $('#dog').animate({'left':'0vw'}, 500);
+  var id = '#b'+$(this).attr('id');
+	$(id).fadeIn(1000);
+  $('.blur').fadeIn(500);
+
+	$('.blur,#dog').click(function() {
+ 		$('#dog').animate({'left':'-100vw'},500);
+		$(id).fadeOut(500);
+    $('.blur').fadeOut(500);
+	})
+})
+
+
