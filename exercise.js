9909d6f7f66f641364340517e787e98105a175bb
hzaybr <ahyebear@gmail.com>
2021-04-06 18:15:56 +0800

    add topbar

--- exercise.js
+++ exercise.js
@@ -4,7 +4,7 @@ $('.pic').click(function() {
 	$(id).fadeIn(1000);
   $('.blur').fadeIn(500);
 
-	$('.blur,#dog').click(function() {
+	$('.blur').click(function() {
  		$('#dog').animate({'left':'-100vw'},500);
 		$(id).fadeOut(500);
     $('.blur').fadeOut(500);
