30129e15b1b52f135143a7b70cd77e1dbfbeaf49
barryfang0531 <barryfang0531@gmail.com>
2021-04-08 00:20:12 +0800

    fix animation

--- exercise.js
+++ exercise.js
@@ -7,8 +7,14 @@ $('.pic').click(function() {
 	$(pic).fadeIn(2000);
 	$(intro).fadeIn(2000);
 
-	$('.blur').click(function() {
- 		$('#dog').animate({'left':'-100vw'},500);
+  $('.blur').click(function() {
+    //add stop
+    $('#dog').stop();
+    $('.blur').stop();
+	  $(pic).stop();
+    $(intro).stop();
+    //
+    $('#dog').animate({'left':'-100vw'},500);
     $('.blur').fadeOut(500);
 		$(pic).fadeOut(500);
 		$(intro).fadeOut(500);
