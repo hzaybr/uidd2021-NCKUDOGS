4212456d88c31560d293179d13a87db619c26940
hzaybr <ahyebear@gmail.com>
2021-04-07 21:27:16 +0800

    add intro

--- exercise.js
+++ exercise.js
@@ -1,13 +1,17 @@
 $('.pic').click(function() {
   $('#dog').animate({'left':'0vw'}, 500);
-  var id = '#b'+$(this).attr('id');
-	$(id).fadeIn(1000);
+  var pic = '#b'+$(this).attr('id');
+  var intro = '#w'+$(this).attr('id');
+  console.log(pic)
   $('.blur').fadeIn(500);
+	$(pic).fadeIn(2000);
+	$(intro).fadeIn(2000);
 
 	$('.blur').click(function() {
  		$('#dog').animate({'left':'-100vw'},500);
-		$(id).fadeOut(500);
     $('.blur').fadeOut(500);
+		$(pic).fadeOut(500);
+		$(intro).fadeOut(500);
 	})
 })
 
