243e42ce41deca980df9d8bcd72e054d287bba67
hzaybr <ahyebear@gmail.com>
2021-04-22 17:17:05 +0800

    feat: new aboutdog

--- aboutus_appv/about.js
+++ aboutus_appv/about.js
@@ -1,16 +1,17 @@
+var pic
+var intro
+
 $('.avatar').click(function() {
-  var pic = '#b'+$(this).attr('id');
-  var intro = '#w'+$(this).attr('id');
+  pic = '#b'+$(this).attr('id');
+  intro = '#w'+$(this).attr('id');
   console.log(pic,intro)
-  $('.blur').fadeIn(500);
+  $('.blur').fadeIn(300);
   $(pic).fadeIn(500);
   $(intro).fadeIn(500);
-
-  $('.blur').click(function() {
-    $('.blur').fadeOut(500);
-	  $(pic).fadeOut(500);
-	  $(intro).fadeOut(500);
-  })
 })
-
+$('.blur').click(function() {
+  $('.blur').fadeOut(500);
+  $(pic).fadeOut(500);
+  $(intro).fadeOut(500);
+})
 
