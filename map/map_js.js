6a783701e678e3fcf6b4c8fdbb37ae58c2341007
Hsuan <lemon861026@gmail.com>
2021-04-28 13:57:17 +0800

    get user position

--- map/map_js.js
+++ map/map_js.js
@@ -1,5 +1,6 @@
 var map;
-var marker;
+var ownermarker;
+var owner_uluru = {lat: 23, lng: 120.2};
 // var Markers=[];
 function initMap() {
     var uluru = {lat: 23.0, lng: 120.21986287979763};
@@ -14,19 +15,19 @@ function initMap() {
     map.addListener('click', function(event){
         addMarker(event.latLng);
     });
-    var infowincontent = '<div style="width:200px">CONTENT</div>';
-    var marker_old = new google.maps.Marker({
-      position: uluru,
+    // var infowincontent = '<div style="width:200px">CONTENT</div>';
+    ownermarker = new google.maps.Marker({
+      position: owner_uluru,
       map: map
     });
-    var infowindow0 = new google.maps.InfoWindow({
-      content: infowincontent.replace('CONTENT',
-        'library'
-      )
-    });
-    marker_old.addListener('click', function() {
-        infowindow0.open(map, marker_old);
-    });
+    // var infowindow0 = new google.maps.InfoWindow({
+    //   content: infowincontent.replace('CONTENT',
+    //     'library'
+    //   )
+    // });
+    // marker_old.addListener('click', function() {
+    //     infowindow0.open(map, marker_old);
+    // });
     
 }
 function addMarker(location) {
@@ -37,3 +38,19 @@ function addMarker(location) {
     map: map
     });
 }
+function findposition(target_marker){
+  navigator.geolocation.getCurrentPosition((position) =>{
+    console.log(position.coords);
+    lat=position.coords.latitude;
+    lng=position.coords.longitude;
+    uluru = {lat: lat, lng: lng};
+    target_marker.setPosition(uluru);
+    map.setCenter(uluru);
+    map.setZoom(18);
+  });
+}
+$( "#mark-icon" ).click(function() {
+  // var pick=$(this).attr('id');
+  // console.log($(this).attr('id'));
+  findposition(ownermarker);
+});
