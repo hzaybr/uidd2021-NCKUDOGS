07d3eba660a09416e9b0f510136a5db690933630
hzaybr <ahyebear@gmail.com>
2021-05-07 02:19:40 +0800

    get user photo

--- dogprofile/user.js
+++ dogprofile/user.js
@@ -1,14 +1,16 @@
 var address = location.href
 console.log(address)
-
-let name = address.match(/=(\W|\w|\z)*/)[0].slice(1)
+//get user name
+let name = address.match(/user=(\W|\w|\z)*&/)[0].slice(5,-1)
 name = decodeURI(name)
-console.log(name)
+console.log(`user name: ${name}`)
+
+//get user profile photo (address)
+let pic_address = address.match(/pic=(\W|\w|\z)*/)[0].slice(4,-1)
+console.log(`pic address: ${pic_address}`)
+pic_address = decodeURI(pic_address)
+pic_address = atob(pic_address)
+console.log(`pic address: ${pic_address}`)
 
 $('.username').attr('id',name)
 
-var jsonfile = require('jsonfile');
-for (i=0; i <11 ; i++){
-  console.log('write')
-     jsonfile.writeFile('loop.json', "id :" + i + " square :" + i*i);
-   }
