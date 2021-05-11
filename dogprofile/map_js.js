aa7023c7266757657103d444b6ab6f50cf9cbfa6
IzsKon <eric246871@gmail.com>
2021-05-12 02:03:21 +0800

    Change redir_url in map_js.js

--- dogprofile/map_js.js
+++ dogprofile/map_js.js
@@ -19,7 +19,7 @@ var Markers=[];
 var Infowincontents=[];
 var count = -1;
 var target_num;
-var redir_url = location.href.match(/.html(\W|\w|\z)*/)[0].slice(6);
+var redir_url = location.href.match(/.html(\W|\w|\z)*&/)[0].slice(6).slice(0,-1);;
 var dog_name = ['豆皮','小小乖','跳跳','皮蛋','白米','米香','麵線','呆呆','阿勇','小武','阿貴','奶茶','豆豆','仙草','黑熊','豆腐','北極熊','棕熊','拉拉'];
 var position = {
   "1":{
