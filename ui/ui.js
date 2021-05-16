49fe1cd3c3f112c706b5b00f9e10970cf46ecf36
barryfang0531 <barryfang0531@gmail.com>
2021-05-17 00:34:06 +0800

    set profile display none at first

--- ui/ui.js
+++ ui/ui.js
@@ -45,11 +45,11 @@ function listClick(){
 function profileClick(){
   $('.profile').stop();
   if (profileDisplay){
-    $('.profile').animate({'left':'105vw'},500);
+    $('.profile').animate({'left':'105vw','display':'none'},500);
   }
   else{
     displayCheck();
-    $('.profile').animate({'left':'3.73vw'},500);
+    $('.profile').animate({'left':'3.73vw','display':'block'},500);
   }
   profileDisplay = !profileDisplay;
 
